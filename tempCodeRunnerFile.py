a3923da75c773696beeee6936581a424eb4e6a01
TanmaTotla <tanmaytotla@gmail.com>
2024-09-05 03:07:20 +0530

    first commit

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-try:
-        combined_text = ''.join(text)  # Combine individual characters into a single string
-        translated_text = translator.translate(combined_text, dest=selected_language).text
-
-    except ValueError as e:
-        return f"Error: {e}"
