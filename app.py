56e3e6b36690b4b9ac3efd8820fcb1614ecf347d
TanmayTotla <94099569+TanmayTotla@users.noreply.github.com>
2024-09-05 14:59:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -232,6 +232,6 @@ def process_image():
         mimetype='image/jpeg'
     )
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
